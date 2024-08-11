4c3935f5b64bcf72834f322f886046e77f10d023
Phanus Thitratsamee <jean@thailandwebsolution.com>
2024-08-12 00:30:13 +0700

    Update CourseController.java

--- src/main/java/com/cloudtechmasters/CourseController.java
+++ src/main/java/com/cloudtechmasters/CourseController.java
@@ -3,38 +3,19 @@ package com.cloudtechmasters;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 
-/**
- * Controller for handling course-related endpoints.
- */
 @RestController
 public class CourseController {
-
-    /**
-     * Endpoint to retrieve a list of AWS services.
-     *
-     * @return A list of AWS services.
-     */
+//create two endpoints getAllAwsServices and getAllDevopsTools
     @GetMapping("/getAllAwsServices")
-    public List<String> getAllAwsServices() {
-        // Using a mutable list to avoid UnsupportedOperationException
-        List<String> awsServices = new ArrayList<>(Arrays.asList("ec2", "iam", "rds", "ecr", "eks"));
-        awsServices.add("s3"); // Adding an additional service
-        return awsServices;
+    public List<String> getAllAwsServices(){
+        return Arrays.asList("ec2","iam","rds","ecr","eks");
     }
 
-    /**
-     * Endpoint to retrieve a list of DevOps tools.
-     *
-     * @return A list of DevOps tools.
-     */
     @GetMapping("/getAllDevopsTools")
-    public List<String> getAllDevopsTools() {
-        // Using a mutable list to provide more flexibility
-        return new ArrayList<>(Arrays.asList("git", "maven", "sonar", "nexus", "jenkins"));
+    public List<String> getAllDevopsTools(){
+        return Arrays.asList("git","maven","sonar","nexus","jenkins");
     }
 }
